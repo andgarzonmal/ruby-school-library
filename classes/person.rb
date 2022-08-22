58d04a78135a81d6553021af951ebbf2dec1d94e
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-22 16:30:13 -0500

    refactor person

--- classes/person.rb
+++ classes/person.rb
@@ -3,7 +3,7 @@ class Person
   attr_reader :id
 
   def initialize(age, name = 'Unknow', parent_permission: true)
-    @id = rand(1...500)
+    @id = rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
