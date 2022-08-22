43f90a49415fde6e1aac7234134e2ada5f0ddd7a
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-22 10:42:18 -0500

    set id only as a getter

--- classes/person.rb
+++ classes/person.rb
@@ -1,5 +1,6 @@
 class Person
-  attr_accessor :id, :name, :age
+  attr_accessor :name, :age
+  attr_reader :id
 
   def initialize(age, name = 'Unknow', parent_permission: true)
     @id = rand(1...500)
