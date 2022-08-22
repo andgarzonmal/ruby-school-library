f098a6289930f2a5c1d11d0d946bc09cb2f5e8c5
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-22 16:30:26 -0500

    refactor student.rb

--- classes/student.rb
+++ classes/student.rb
@@ -3,7 +3,7 @@ require_relative './person'
 class Students < Person
   def initialize(classroom, age, name = 'Unknow', parent_permission: true)
     @classroom = classroom
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
   end
 
   def play_hooky
