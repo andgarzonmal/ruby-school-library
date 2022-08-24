a3b545129ac3a732e343a7b0d699845f40e7ef77
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-24 15:01:08 -0500

    modify student class

--- classes/student.rb
+++ classes/student.rb
@@ -1,6 +1,8 @@
 require_relative './person'
 
 class Students < Person
+  attr_reader :classroom
+
   def initialize(classroom, age, name = 'Unknow', parent_permission: true)
     @classroom = classroom
     super(age, name, parent_permission: parent_permission)
@@ -8,5 +10,11 @@ class Students < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  # Both ways relationship
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
