cfa8b6c8edb464a3c9074d042fd45186b92d93dd
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-25 18:06:40 -0500

    refactor

--- classes/person.rb
+++ classes/person.rb
@@ -1,8 +1,8 @@
 require_relative './nameable'
 
 class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id, :rentals
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
 
   def initialize(age, name = 'Unknow', parent_permission: true)
     super()
