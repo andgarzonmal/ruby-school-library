1af45e9de8e53554b59fd0c7bdea1173a253096b
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-25 18:19:30 -0500

    fix linters

--- classes/app.rb
+++ classes/app.rb
@@ -84,6 +84,6 @@ class App
     puts 'ID of person: '
     selected_id = gets.chomp.to_i
     list_of_rentals = @rentals.select { |rental| rental.person.id == selected_id }
-    list_of_rentals.each {|item| puts "Date: #{item.date}, Book: \"#{item.book.title}\" by #{item.book.author}"}
+    list_of_rentals.each { |item| puts "Date: #{item.date}, Book: \"#{item.book.title}\" by #{item.book.author}" }
   end
 end
