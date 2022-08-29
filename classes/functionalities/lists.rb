0c018d4fe0c4a3b8175867fb6a96097f5ff08623
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-29 15:12:34 -0500

    create List class

---
+++ classes/functionalities/lists.rb
@@ -0,0 +1,16 @@
+class List
+  def list_all_rentals(rentals)
+    puts 'ID of person: '
+    selected_id = gets.chomp.to_i
+    list_of_rentals = rentals.select { |rental| rental.person.id == selected_id }
+    list_of_rentals.each { |item| puts "Date: #{item.date}, Book: \"#{item.book.title}\" by #{item.book.author}" }
+  end
+
+  def list_all_books(books)
+    books.each { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
+  end
+
+  def list_all_people(people)
+    people.each { |person| puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
+  end
+end
