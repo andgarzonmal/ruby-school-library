0bc7c7ccfa4225bbd2ff7d1986eb12d91c6b154b
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-31 00:54:24 -0500

    add methods

--- library/Helper_module/preserveLoad.rb
+++ library/Helper_module/preserveLoad.rb
@@ -1,16 +1,87 @@
 require 'json'
+require_relative '../teacher'
+require_relative '../student'
+require_relative '../book'
+require_relative '../rental'
+require_relative '../person'
 
-module preserveLoad
+module PreserveLoad
   def store_all_books
-    File.write('../Data/books.json', @books.to_json)
+    data = @books.map { |book| { title: book.title, author: book.author } }
+    File.write('library/Data/books.json', JSON.pretty_generate(data))
   end
-  
+
   def store_all_people
-    File.write('../Data/people.json', @people.to_json)
+    data = @people.map do |person|
+      if person.instance_of?(Student)
+        { class: person.class, name: person.name, age: person.age, id: person.id,
+          parent_permission: person.parent_permission, rentals: [] }
+      elsif person.class != Student
+        { class: person.class, age: person.age, name: person.name, specialization: person.specialization,
+          id: person.id, rentals: [] }
+      end
+    end
+    File.write('library/Data/people.json', JSON.pretty_generate(data))
   end
 
   def store_all_rentals
-    File.write('../Data/rentals.json', @people.to_json)
+    data = @rentals.map do |rental|
+      if rental.person.instance_of?(Student)
+        { date: rental.date,
+          book: { title: rental.book.title, author: rental.book.author },
+          person: { name: rental.person.name, age: rental.person.age, id: rental.person.id,
+                    parent_permission: rental.person.parent_permission, class: rental.person.class, rentals: rental.person.rentals  }         
+        }
+      elsif rental.person.class != Student
+        { date: rental.date,
+          book: { title: rental.book.title, author: rental.book.author },
+          person: { name: rental.person.name, age: rental.person.age, id: rental.person.id,
+                    specialization: rental.person.specialization, class: rental.person.class, rentals: rental.person.rentals }
+        }
+      end
+    end
+    File.write('library/Data/rentals.json', JSON.pretty_generate(data))
   end
+
+  def create_all_books 
+    @books = []
+    file = File.open('library/Data/books.json')
+    if !File.zero?('library/Data/books.json')
+      file_data = file.read
+     data_arr = JSON.parse(file_data, symbolize_names:true)
+     if data_arr.length > 0
+       data_arr.each {|book| @books << Book.new(book[:title], book[:author])}
+      end
+    end
+  end 
+
+  def create_all_people
+    @people = []
+    file = File.open('library/Data/people.json')
+    if !File.zero?('library/Data/people.json')
+      file_data = file.read
+      data_arr = JSON.parse(file_data, symbolize_names:true)
+      if data_arr.length > 0
+        data_arr.each do |person|
+          if person[:class] == "Student"
+            @people << Student.new('clase_1', person[:age], person[:name], parent_permission: person[:parent_permission])
+          elsif person[:class] != "Student"
+            @people << Teacher.new(person[:specialization], person[:age], person[:name])
+          end
+        end  
+       end
+    end
+  end 
   
+  def create_all_rentals 
+    @rentals = []
+    file = File.open('library/Data/rentals.json')
+    if !File.zero?('library/Data/rentals.json')
+      file_data = file.read
+      data_arr = JSON.parse(file_data, symbolize_names:true)
+      if data_arr.length > 0
+        data_arr.each {|rental| @rentals << Rental.new(rental[:date], rental[:person], rental[:book])}
+      end
+    end
+  end
 end
