401bc272cb73e5eb92028efca62dcd844d08bc42
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-31 16:56:26 -0500

    add tests for students

--- spec/tests/student_spec.rb
+++ spec/tests/student_spec.rb
@@ -3,15 +3,21 @@ require_relative '../../library/student'
 describe Student do
   student = Student.new('class_1', 55, 'andres', parent_permission: 'y')
   
- it 'Contains a student name age id parent_permission' do
-  expect(student.classroom).to eq('class_1') 
- end
+  it 'Contains a student name age id parent_permission' do
+    expect(student.classroom).to eq('class_1') 
+  end
 
- it 'student that contains age parameter' do
-  expect(student.age).to eq(55)
+  it 'student that contains age parameter' do
+    expect(student.age).to eq(55)
+  end
 
- end
+  it 'book contains both arguments' do
+    expect(student).to be_instance_of Student
+  end
 
+  it 'student has method play_hooky' do 
+    expect(student.play_hooky).to eq("¯(ツ)/¯")
+  end
 end
 
 # classroom, age, name = 'Unknow', id = nil, parent_permission: true
