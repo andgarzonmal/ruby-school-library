32cb5633061eedf343b541599456c398bb83ae6d
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-31 15:37:36 -0500

    add tests to book

--- spec/tests/book_spec.rb
+++ spec/tests/book_spec.rb
@@ -1,2 +1,17 @@
 require_relative '../../library/book'
 
+describe 'book is created' do 
+  book = Book.new("Game of thrones", "George RR Martin") 
+  
+  it 'book contains title' do
+    expect(book.title).to eq("Game of thrones")
+  end
+
+  it 'book contains author' do 
+    expect(book.author).to eq("George RR Martin")
+  end
+
+  it 'book contains both arguments' di 
+    expect(book).to be_instance_of 
+  end
+end
