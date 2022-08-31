1772b72bd5dd6926e12926373163b6912347bab8
oyelakinG9 <oyelakinr37@gmail.com>
2022-08-31 23:10:24 +0100

    Modify teacher spec rb file

--- spec/tests/teacher_spec.rb
+++ spec/tests/teacher_spec.rb
@@ -1,1 +1,21 @@
 require_relative '../../library/teacher'
+
+describe Teacher do 
+  teacher = Teacher.new('Developer', 30, 'Ariel')
+
+  it 'Parameters for the instance of teacher' do 
+    expect(teacher).to be_instance_of Teacher
+  end
+
+  it 'Parameter: name of teacher' do 
+    expect(teacher.name).to eq('Ariel')
+  end
+
+  it 'Parameter: name of teacher' do 
+    expect(teacher.age).to eq(30)
+  end
+
+  it 'Parameter: name of teacher' do 
+    expect(teacher.can_use_services?).to eq(true)
+  end
+end
