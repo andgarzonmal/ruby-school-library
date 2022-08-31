b613a7c7ea6b377fd2104b9de7b56cefb30fb329
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-31 16:56:11 -0500

    add test for methods and variables on person

---
+++ spec/tests/person_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../../library/person'
+
+describe Person do 
+  person = Person.new(35, 'Andres', 20, parent_permission: true)
+
+  it 'person contains age' do 
+    expect(person.age).to eq(35)
+  end
+
+  it 'person contains main' do
+    expect(person.name).to eq('Andres')
+  end
+
+  it 'person contains name method' do
+    expect(person.correct_name).to eq('Andres')
+  end
+
+  it 'person contains of age method' do
+    expect(person.can_use_services?).to eq(true)
+  end
+end
