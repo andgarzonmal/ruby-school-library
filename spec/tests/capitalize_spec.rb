622d94d7784586cf17014eeed825345a9c080865
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-31 20:18:03 -0500

    add tests to decorators

---
+++ spec/tests/capitalize_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../../library/capitalize_decorator'
+require_relative '../../library/person'
+
+describe CapitalizeDecorator do
+  person = Person.new(35, 'andres', 20, parent_permission: true)
+  capitalized = CapitalizeDecorator.new(person)
+  
+  it 'capitalized is an instance' do
+    expect(capitalized).to be_instance_of CapitalizeDecorator
+  end
+
+  it 'returns the person name capitalized' do
+    expect(capitalized.correct_name).to eq('Andres')
+  end
+end
