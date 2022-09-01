a9b037d25a2c3706d55d142e47f8cf58bcb8c423
andgarzonmal <andgarzonmal@unal.edu.co>
2022-08-31 20:18:39 -0500

    Fix linters

--- spec/tests/capitalize_spec.rb
+++ spec/tests/capitalize_spec.rb
@@ -4,7 +4,7 @@ require_relative '../../library/person'
 describe CapitalizeDecorator do
   person = Person.new(35, 'andres', 20, parent_permission: true)
   capitalized = CapitalizeDecorator.new(person)
-  
+
   it 'capitalized is an instance' do
     expect(capitalized).to be_instance_of CapitalizeDecorator
   end
--- spec/tests/trimmer_spec.rb
+++ spec/tests/trimmer_spec.rb
@@ -4,7 +4,7 @@ require_relative '../../library/person'
 describe TrimmerDecorator do
   person = Person.new(35, 'maximilianus', 20, parent_permission: true)
   trimmed = TrimmerDecorator.new(person)
-  
+
   it 'trimmed is an instance' do
     expect(trimmed).to be_instance_of TrimmerDecorator
   end
